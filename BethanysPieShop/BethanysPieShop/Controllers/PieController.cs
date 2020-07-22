f94b3f54e05a9567cf333ad95ad8664b5c7a2b69
dsarghe@gmail.com <dsarghe@gmail.com>
2020-07-22 10:21:25 +0300

    final

---
+++ BethanysPieShop/BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        //public ViewResult List()
+        //{
+        //    PieListViewModel pieListViewModel = new PieListViewModel();
+        //    pieListViewModel.Pies = _pieRepository.AllPies;
+
+        //    pieListViewModel.CurrentCategory = "Cheese cake";
+        //    return View(pieListViewModel);
+        //}
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieByID(id);
+
+            if (pie == null) return NotFound();
+
+            return View(pie);
+        }
+        public ViewResult List(string category)
+        {
+            IEnumerable<Pie> pies;
+            string currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.Name);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.Name);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new PieListViewModel
+            {
+                CurrentCategory = currentCategory,
+                Pies = pies
+            });
+
+        }
+    }
+}
